011fd3934fa8301159ffa57140e9a373af8398cd
Thomas Bower <me@thomasbower.net>
2017-03-11 19:18:20 +0000

    Fontset now gets loaded in to memory

--- src/cpu.c
+++ src/cpu.c
@@ -1,30 +1,55 @@
 #include "cpu.h"
 
+/* Chip 8 basic font set */
+uint8_t fontset[80] = {
+  0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+  0x20, 0x60, 0x20, 0x20, 0x70, // 1
+  0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+  0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+  0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+  0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+  0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+  0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+  0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+  0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+  0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+  0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+  0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+  0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+  0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+  0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+};
+
 int main(int argc, char **argv) {
-  struct machine *machine;
+  struct machine *machine = malloc(sizeof(struct machine));
+  
+  assert(machine != NULL);
+  
   machine_init(machine);
 }
 
-void machine_init(struct machine *machine) {
+void machine_init(struct machine *m) {
   // TODO: put an assert here checking if null
   
   /* Initialise positions of pointer registers */
-  machine->i = 0;
-  machine->pc = 0x200;
-  machine->sp = 0;
+  m->i = 0;
+  m->pc = 0x200;
+  m->sp = 0;
   
   /* Reset the timers */
-  machine->delay_timer = 0;
-  machine->sound_timer = 0;
+  m->delay_timer = 0;
+  m->sound_timer = 0;
   
   /* Clear display, registers, keys, stack, and memory */
-  memset(machine->display, 0, sizeof(machine->display));
-  memset(machine->keys, 0, sizeof(machine->keys));
-  memset(machine->V, 0, sizeof(machine->V));
-  memset(machine->stack, 0, sizeof(machine->stack));
-  memset(machine->memory, 0, sizeof(machine->memory));
+  memset(m->display, 0, sizeof(m->display));
+  memset(m->keys, 0, sizeof(m->keys));
+  memset(m->V, 0, sizeof(m->V));
+  memset(m->stack, 0, sizeof(m->stack));
+  memset(m->memory, 0, sizeof(m->memory));
 
-  // TODO: load the fontset into memory
-  //       between 0x050 and 0x0A0
+  /* Load fontset into memory at the expected location */
+  memcpy(&m->memory[0x050], fontset, sizeof(fontset));
 
 }
+
+
